267322ba357078809fa0bb219d82f6b7145b39ed
Eyre_S <Sukazyo@outlook.com>
2020-11-21 00:24:17 +0800

    添加生物骨头掉落，烈焰物品制作，Item生成工具类更新
    - 添加烈焰粉和烈焰棒的制作
    - 添加深生物骨头掉落
      |  生物  | 掉落量 |
      -----------------
      | 北极熊 |  1-3  |
      |  羊驼  |  1-3  |
      |   牛   |  0-2  |
      |   羊   |  0-2  |
      |   猪   |  0-2  |
      |   马   |  0-2  |
      |   驴   |  0-2  |
      |   骡   |  0-2  |
      |   鸡   |  0-1  |
      |   兔   |  0-1  |
      |   狼   |  0-1  |
      |  豹猫  |  0-1  |
      |  鹦鹉  |  0-1  |
    - 更新 ItemSummon 工具类
      - 将三数值的构造方法从含meta改为数值区间定义（鉴于平常的meta使用并不多）
      - 将 ItemSummonTable 的子对象 Stack 独立为 ItemSummonStack
        - 同时也给 ItemSummonStack 添加了带有扩展数量的生成方法以适应掠夺等方式的生成

---
+++ src/main/java/cc/sukazyo/entityalchemy/struct/ItemSummonStack.java
@@ -0,0 +1,33 @@
+package cc.sukazyo.entityalchemy.struct;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.Random;
+
+public class ItemSummonStack {
+	
+	private final Item item;
+	private final int meta;
+	
+	private final int numMax;
+	private final int numMin;
+	
+	public ItemSummonStack(Item item, int num) { this(item, num, num); }
+	public ItemSummonStack(Item item, int numMin, int numMax) { this(item, numMin, numMax, 0); }
+	public ItemSummonStack(Item item, int numMin, int numMax, int meta) {
+		this.item = item;
+		this.meta = meta;
+		this.numMin = numMin;
+		this.numMax = numMax;
+	}
+	
+	public ItemStack summon () {
+		return new ItemStack(item, new Random().nextInt(numMax-numMin+1) + numMin, meta);
+	}
+	
+	public ItemStack summonWithExtraChance (int extra) {
+		return new ItemStack(item, new Random().nextInt(numMax - numMin + extra + 1) + numMin, meta);
+	}
+	
+}
--- src/main/java/cc/sukazyo/entityalchemy/struct/ItemSummonTable.java
+++ src/main/java/cc/sukazyo/entityalchemy/struct/ItemSummonTable.java
@@ -5,46 +5,24 @@ import net.minecraft.item.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class ItemSummonTable {
 	
-	private final List<Stack> table;
-	
-	public static class Stack {
-		
-		private final Item item;
-		private final int meta;
-		
-		private final int numMax;
-		private final int numMin;
-		
-		Stack (Item item, int numMin, int numMax, int meta) {
-			this.item = item;
-			this.meta = meta;
-			this.numMin = numMin;
-			this.numMax = numMax;
-		}
-		
-		private ItemStack summon () {
-			return new ItemStack(item, new Random().nextInt(numMax-numMin+1) + numMin, meta);
-		}
-		
-	}
+	private final List<ItemSummonStack> table;
 	
 	public ItemSummonTable () { table = new ArrayList<>(); }
 	
-	public ItemSummonTable (Item item, int num) { this(item, num, 0); }
-	public ItemSummonTable (Item item, int num, int meta) { this(item, num, num, meta); }
+	public ItemSummonTable (Item item, int num) { this(item, num, num); }
+	public ItemSummonTable (Item item, int numMin, int numMax) { this(item, numMin, numMax, 0); }
 	public ItemSummonTable (Item item, int numMin, int numMax, int meta) {
 		table = new ArrayList<>();
-		table.add(new Stack(item, numMin, numMax, meta));
+		table.add(new ItemSummonStack(item, numMin, numMax, meta));
 	}
 	
-	public ItemSummonTable add (Item item, int num) { return add(item, num, 0); }
-	public ItemSummonTable add (Item item, int num, int meta) { return add(item, num, num, meta); }
+	public ItemSummonTable add (Item item, int num) { return add(item, num, num); }
+	public ItemSummonTable add (Item item, int numMin, int numMax) { return add(item, numMin, numMax, 0); }
 	public ItemSummonTable add (Item item, int numMin, int numMax, int meta) {
-		table.add(new Stack(item, numMin, numMax, meta));
+		table.add(new ItemSummonStack(item, numMin, numMax, meta));
 		return this;
 	}
 	
