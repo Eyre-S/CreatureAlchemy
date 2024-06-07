0c805207dded443ae0de6cecdff695e20a9c8b55
Eyre_S <sukazyo@outlook.com>
2024-06-08 01:55:11 +0800

    Brand new Creature Alchemy with 1.20.1 Fabric!

---
+++ README.md
@@ -0,0 +1,18 @@
+# Carpet Nukos Addition
+
+A carpet extension/addition provides features that are used in Nukoland
+server.
+
+**This mod is under WIP status.**
+
+## Scheduled features list
+
+- [x] anvilItemCostRollupAlgorithm
+- [ ] anvilCostAlgorithm
+- [ ] anvilChangeNameCost
+- [x] anvilItemCostRollupAlgorithm
+- [ ] tickFreezeWhenNoPlayersUseDeepFreeze
+- [ ] petsPreventOwnerDamage
+- [ ] petsShowHealth
+- [ ] petsTransferable
+- [ ] more pets features...
---
+++ src/main/java/cc/sukazyo/creature_alchemy/CreatureAlchemy.java
@@ -0,0 +1,34 @@
+package cc.sukazyo.creature_alchemy;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CreatureAlchemy implements ModInitializer {
+	
+	public static final String MOD_ID = "creature_alchemy";
+	public static final String NAME = "Creature Alchemy";
+	private static String _version;
+	public static String version () { return _version; }
+	
+	public static final Logger logger = LogManager.getLogger(NAME);
+	
+	public CreatureAlchemy () {}
+	
+	@Override
+	public void onInitialize () {
+		
+		// Get version info of this mod
+		_version =
+				FabricLoader.getInstance()
+						.getModContainer(MOD_ID).get()
+						.getMetadata()
+						.getVersion()
+						.toString();
+		
+		logger.info("Hello, Creature Alchemy!");
+		
+	}
+	
+}
